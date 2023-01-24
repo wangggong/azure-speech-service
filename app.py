e5762c2edf953c3529bb0f0c0d4158f00f367178
wangggong <793160615@qq.com>
2023-01-24 18:17:21 +0800

    fix: fix FE bug

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config.from_json("config.json")
 @app.route('/')
 def index():
     print('Request for index page received')
-    return render_template('txt2speech.html')
+    return render_template('index.html')
 
 
 @app.route('/favicon.ico')
@@ -28,18 +28,6 @@ def favicon():
     return send_from_directory(
         os.path.join(app.root_path, 'static'),
         'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-
-@app.route('/hello', methods=['POST'])
-def hello():
-    name = request.form.get('name')
-
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return render_template('hello.html', name = name)
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return redirect(url_for('index'))
 
 
 @app.route('/txt2speech', methods=['GET', 'POST'])
